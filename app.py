abf6f16b3eab4fdb8193f3b4d70b392beebb1749
jspearus <35698087+jspearus@users.noreply.github.com>
2024-10-25 15:01:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,51 +5,14 @@ from flask import Flask, render_template, request, jsonify
 from waitress import serve
 import os
 import json
-import Adafruit_SSD1306
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
 
 import subprocess
-
+from screen.py import refreshScreen
 from ctrl import displayTheme, off
 from theme_handler import  create_theme, get_all_themes
 from events_handler import create_event, get_next_event, get_all_events
 from config import get_config, edit_config
 
-
-numb = 1
-RST = None
-
-# Create the SSD1306 OLED class.
-# The first two parameters are the width and height. The third is the I2C interface.
-# oled = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
-# # Initialize library.
-# oled.begin()
-
-# # Clear display.
-# oled.clear()
-# oled.display()
-
-
-# # Create blank image for drawing.
-# # Make sure to create image with mode '1' for 1-bit color.
-# width = oled.width
-# height = oled.height
-# image = Image.new('1', (width, height))
-
-# # Get drawing object to draw on image.
-# draw = ImageDraw.Draw(image)
-# # First define some constants to allow easy resizing of shapes.
-# padding = 10
-# top = padding
-# bottom = height-padding
-# # Move left to right keeping track of the current x position for drawing shapes.
-# x = 0
-
-
-# # Load default font.
-# font = ImageFont.load_default()
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
@@ -131,28 +94,6 @@ def ctrl():
     elif data['cmd'] in themes:
         displayTheme(data['cmd'])
     return jsonify({'status': 'success'})
-
-def refreshScreen():
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True ).decode('ASCII')
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell = True ).decode('ASCII')
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell = True ).decode('ASCII')
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True ).decode('ASCII')
-
-    # Write two lines of text.
-
-    draw.text((x, top),       "     mini TREE",  font=font, fill=255)
-    # draw.text((x, top+12),     str(CPU), font=font, fill=255)
-    # draw.text((x, top+20),    str(MemUsage),  font=font, fill=255)
-    draw.text((x, top+29),"IP: " + str(IP),  font=font, fill=255)
-    draw.text((x, top+39),    "PORT: 8080",  font=font, fill=255)
-    # Display image.
-    oled.image(image)
-    oled.display()
 
 if __name__ == '__main__':
     refreshScreen()
