ed64004a84183f35ea60746f36b702786faaa227
jspearus <jspearus@gmail.com>
2024-12-09 12:01:38 -0600

    update

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 
 
 def get_config():
-    with open('/home/jeff/minitree/config.json', 'r') as outputfile:
+    with open('/home/mini/minitree/config.json', 'r') as outputfile:
             return json.load(outputfile)
 
 def edit_config(config):
-    with open('/home/jeff/minitree/config.json', 'w', encoding='utf-8') as f:
+    with open('/home/mini/minitree/config.json', 'w', encoding='utf-8') as f:
         f.write(config)
